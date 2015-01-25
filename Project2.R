c071702279572f660af002389ebfe3016d9ab82f
Brian Holt <brian.c.holt@gmail.com>
2015-01-25 13:42:05 -0800

    finish Pro2

--- Project2.R
+++ Project2.R
@@ -1,11 +1,9 @@
 #######
 ##libraries
 #######
-library(data.table)
 library(reshape2)
 library(tidyr)
 library(dplyr)
-library(lattice)
 library(lubridate)
 library(ggplot2)
 getwd()
@@ -75,7 +73,8 @@ boxplot(NEI$Emissions~NEI$year,outline=F)
 Q1table<-tapply(NEI$Emissions/1000,NEI$year,sum)
 png(file ="./images/Q1.png",bg="white")
 
-plot(Q1table,type="b",ylab="Total Emissions by 1000 tons",xlab="Year",main="U.S. Total Emissions by Year",xaxt = "n")
+plot(Q1table,type="b",ylab="Total Emissions by 1000 tons",xlab="Year",
+     main="U.S. Total Emissions by Year",xaxt = "n")
 axis(1,1:4,c("1999", "2002", "2005", "2008"))
 points(Q1table,col="blue",pch=19)
 
@@ -95,10 +94,13 @@ dev.off()  ## Don't forget to close the PNG device!
 #####################
 #Create line chart
 #####################
-Q2table<-tapply(NEI$Emissions[NEI$fips == 24510]/1000,NEI$year[NEI$fips == 24510],sum)
+Q2table<-tapply(NEI$Emissions[NEI$fips == 24510]/1000,
+                NEI$year[NEI$fips == 24510],sum)
+
 png(file ="./images/Q2.png",bg="white")
 
-plot(Q2table,type="l",ylab="Total Emissions",xlab="Year",main = "Emissions for Baltimore, MD",xaxt = "n")
+plot(Q2table,type="l",ylab="Total Emissions",xlab="Year",
+     main = "Emissions for Baltimore, MD",xaxt = "n")
 axis(1,1:4,c("1999", "2002", "2005", "2008"))
 points(Q2table,col="blue",pch=19)
 
@@ -127,7 +129,9 @@ final<-(dcast(final,year+type~variable,sum))
 Q3ggplot<-ggplot(data=final, aes(x=year, y=Emissions ,group=type,color=type))
 summary(Q3ggplot)
 png(file ="./images/Q3.png",bg="white",width = 960, height =480)
-Q3ggplot+geom_line()+geom_point()+facet_wrap(~type,nrow = 1, ncol = 4)+ylab("Emissions, Tons/1000") + ggtitle("Baltimore, MD PM2.5 Emissions by Year, color by Type")
+Q3ggplot+geom_line()+geom_point()+facet_wrap(~type,nrow = 1, ncol = 4)+
+  ylab("Emissions, Tons/1000") + 
+  ggtitle("Baltimore, MD PM2.5 Emissions by Year, color by Type")
 
 
 #####################
@@ -184,23 +188,34 @@ dev.off()  ## Don't forget to close the PNG device!
 #How have emissions from motor vehicle sources changed from
 #1999–2008 in Baltimore City?
 
-#Repeat #3, but use short.name to find motor vehicles?
-length(grep("motor",SCC$Short.Name,ignore.case=T,value=T))
+#Select Baltimore
+df4<-subset(NEI, fips == 24510, select = c(SCC,Emissions, year, type))
 
-head(SCC[grep("highway",SCC$Short.Name,ignore.case=T,value=F),])
+#length(grep("motor",SCC$Short.Name,ignore.case=T,value=T))
+#length(grep("highway",SCC$Short.Name,ignore.case=T,value=F))
 
-length(grep("veh",SCC$Short.Name,ignore.case=T,value=T))
+motor_veh_grep<-grep("veh|motor",SCC$Short.Name,ignore.case=T,value=F)
+SCC_motor<-SCC[motor_veh_grep,]
+SCC_motor<-SCC_motor[SCC_motor$Data.Category=="Onroad",]
 
-table(SCC$SCC.Level.Two)
+final_SCC_motor<-subset(SCC_motor,select = c("SCC","Short.Name"))
 
-tail(SCC[grep("vehicle",SCC$SCC.Level.Two,ignore.case=T),],20)
+NEI_SCC_motor<-merge(df4,final_SCC_motor,by.x="SCC",by.y="SCC")
 
-subset(SCC,select = c("SCC","Short.Name",))
+final<-melt(NEI_SCC_motor, id=c("year", "type"), measure.vars=c("Emissions"))
 
+final<-(dcast(final,year~variable,sum))
 
+Q5ggplot<-ggplot(data=final, aes(x=year, y=Emissions))
+summary(Q5ggplot)
+png(file ="./images/Q5.png",bg="white",width = 480, height =480)
+Q5ggplot+geom_line()+geom_point()+ylab("Emissions, Tons/1000")+ 
+  ggtitle("Baltimore, MD Motor Emissions by Year")
+#####################
+#Copy screen device output to png, saved in ./images/ with w,h at 480 pixels
+#####################
 
-
-
+dev.off()  ## Don't forget to close the PNG device!
 
 #####################
 #Q6
@@ -213,8 +228,43 @@ subset(SCC,select = c("SCC","Short.Name",))
 #
 #
 
+####Prep NEI database, subset out fips not LA or Balt
+#Select Baltimore and LA
+df5=NEI[NEI$fips=="24510"|NEI$fips=="06037",c(1:2,4:6)]
+
+#change fips # to County Name
+df5$fips<-with(df5, ifelse(df5$fips=="24510","Baltimore, MD","Los Angeles, CA"))
+                  
+
+##Grep out vehicles and motors from SCC short.name
+motor_veh_grep<-grep("veh|motor", SCC$Short.Name,ignore.case=T,value=F)
+
+SCC_motor<-SCC[motor_veh_grep,]
+SCC_motor<-SCC_motor[SCC_motor$Data.Category=="Onroad",]
+
+final_SCC_motor<-subset(SCC_motor,select = c("SCC","Short.Name"
+                                             ,"Data.Category", "EI.Sector")
+                                             )
+
+NEI_SCC_Balt_LA_motor<-merge(df5,final_SCC_motor,by.x="SCC",by.y="SCC")
+
+final<-melt(NEI_SCC_Balt_LA_motor, id=c("fips","year","EI.Sector","Data.Category" )
+            , measure.vars=c("Emissions"))
+
+final<-(dcast(final,fips+year+Data.Category+EI.Sector~variable,sum))
+head(final)
+Q6ggplot<-ggplot(data=final, aes(x=year, y=Emissions,color=fips))
+Q6ggplot+geom_line()+geom_point()+facet_wrap(~EI.Sector,ncol = 4)+
+  guides(col = guide_legend(reverse = TRUE))+
+  ylab("Emissions, Tons/1000")+ 
+  ggtitle("LA County, CA vs. Baltimore, MD \nOnroad Motor Emissions by Year")
 
 
+#####################
+#Copy screen device output to png, saved in ./images/ with w,h at 480 pixels
+#####################
+dev.copy(png, file ="./images/Q6.png",bg="white",width = 960, height =480)  ## Copy my plot to a PNG file
+dev.off()  ## Don't forget!
 
 
 
